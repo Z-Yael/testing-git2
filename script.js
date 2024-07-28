c32096e3a9875785cc248041dc7cb39cf381f9ac
zulu-M <zhtet7864@gmail.com>
2024-07-29 00:29:00 +0700

    make my web look sexy

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-console.log("shopping list")
+console.log("shopping list");
 
-const inputTag = document.querySelector('.form-control');
-const shoppingListTag = document.getElementsByClassName('shoppingListContainer')[0];
-//console.log(inputTag)
+const inputTag = document.querySelector(".form-control");
+const shoppingListTag = document.getElementsByClassName(
+  "shoppingListContainer"
+)[0];
 let productId = 1;
+
 const changeFunction = (event) => {
-   // console.log("input change is", event.target.value)
-   const inputValue = event.target.value;
-   //console.log(inputValue)
-   const parentDivTag = document.createElement('div');
-   const parentContainerTag = document.createElement('div')
-   parentContainerTag.classList.add("parentContainer")
-   parentDivTag.classList.add("productName")
-   //<i class="fa-sharp fa-solid fa-trash"></i>
-   parentDivTag.addEventListener('click',()=>{
-    const purchasedClassExist = parentDivTag.classList.contains("purchased");
-    if(purchasedClassExist){
-        parentDivTag.classList.remove("purchased")
-    }else{
-        parentDivTag.classList.add("purchased")
-    }
-   })
-   const spanTag = document.createElement('span');
-   const trashIconTag = document.createElement('i');
-   trashIconTag.classList.add('fa-sharp', 'fa-solid', "fa-trash")
-   trashIconTag.addEventListener('click',(event)=>{
+  const inputValue = event.target.value;
+  const parentContainerTag = document.createElement("div");
+  parentContainerTag.classList.add("parentContainer");
+
+  const parentDivTag = document.createElement("div");
+  parentDivTag.classList.add("productName");
+  parentDivTag.addEventListener("click", () => {
+    parentDivTag.classList.toggle("purchased");
+  });
+
+  const spanTag = document.createElement("span");
+  const trashIconTag = document.createElement("i");
+  trashIconTag.classList.add("fa-sharp", "fa-solid", "fa-trash");
+  trashIconTag.addEventListener("click", () => {
     parentContainerTag.remove();
-   })
-   spanTag.id = productId;
-   const product = productId.toString() + " .  " + inputValue;
-   spanTag.append(product);
-   parentDivTag.append(spanTag)
-   parentContainerTag.append(parentDivTag,trashIconTag)
-   shoppingListTag.append(parentContainerTag)
-   inputTag.value = "";
-   productId += 1;
-}
-inputTag.addEventListener("change", changeFunction)
+  });
+
+  spanTag.id = productId;
+  const product = `${productId} . ${inputValue}`;
+  spanTag.append(product);
+  parentDivTag.append(spanTag);
+  parentContainerTag.append(parentDivTag, trashIconTag);
+  shoppingListTag.append(parentContainerTag);
+  inputTag.value = "";
+  productId += 1;
+};
+
+inputTag.addEventListener("change", changeFunction);
